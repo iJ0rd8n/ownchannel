904c108c8f99dd955656fd21ffbae5f4f7318280
Moi <kane2931@richards-mac-mini.local>
2015-11-11 15:11:38 -0500

    Initial Commit

---
+++ Your Own Channel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Your Own Channel
+//
+//  Created by Richard Sigouin on 2015-11-11.
+//  Copyright © 2015 Richard Sigouin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
