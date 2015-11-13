b90eb5b20a32f5b6368397670266018ca31637b0
Moi <kane2931@richards-mac-mini.local>
2015-11-13 10:43:23 -0500

    1st public release

--- Your Own Channel/ViewController.swift
+++ Your Own Channel/ViewController.swift
@@ -2,24 +2,41 @@
 //  ViewController.swift
 //  Your Own Channel
 //
-//  Created by Richard Sigouin on 2015-11-11.
-//  Copyright © 2015 Richard Sigouin. All rights reserved.
+//  Created by Kane2931 on 2015-11-11.
+//  Copyright © 2015 Kane2931. All rights reserved.
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    // Paste your URL below between the " " to customize your channel (Default: NASA TV).
+    
+    var player = AVPlayer(URL: NSURL(string: "http://nasatv-lh.akamaihd.net/i/NASA_101@319270/master.m3u8")!)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let layer = AVPlayerLayer(player: player)
+        
+        layer.frame = self.view.frame
+        layer.videoGravity = AVLayerVideoGravityResizeAspectFill
+        
+        self.view.layer.addSublayer(layer)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    internal func play() {
+        player.play()
+    }
+    
+    internal func pause() {
+        player.pause()
+    }
+    
 }
-
